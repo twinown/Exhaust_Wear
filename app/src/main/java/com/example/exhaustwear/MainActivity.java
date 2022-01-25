397d033796f4837cfff7d0890276c2ccae84e47c
twinown <niyaz9797@gmail.com>
2022-01-25 13:38:59 +0300

    adding top and bottom bars

--- app/src/main/java/com/example/exhaustwear/MainActivity.java
+++ app/src/main/java/com/example/exhaustwear/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.exhaustwear;
 
+import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+import com.example.exhaustwear.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+BottomNavigationView bottomNavigationView;
+Toolbar toolbar;
+ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        //toolbar makes func. of the actionbar
+        setSupportActionBar(toolbar);
+        setContentView(binding.getRoot());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+
+
+            return true;
+
+        });
+    }
+    //putting the icons in the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_bar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //for searching
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+      if (item.getItemId()==R.id.search){
+            Toast.makeText(this, "Do it later", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
