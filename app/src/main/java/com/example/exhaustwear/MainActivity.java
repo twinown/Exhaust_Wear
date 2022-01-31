47c90137242fbdc4f3abcf461e89da147d40f0ff
twinown <niyaz9797@gmail.com>
2022-01-31 20:56:02 +0300

    losing search icon

--- app/src/main/java/com/example/exhaustwear/MainActivity.java
+++ app/src/main/java/com/example/exhaustwear/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.annotation.NonNull;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,34 +18,63 @@ import android.widget.Toast;
 import com.example.exhaustwear.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-BottomNavigationView bottomNavigationView;
-Toolbar toolbar;
-ActivityMainBinding binding;
 
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    ActivityMainBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setBackground(null);
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("");
+        //for top bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
         //toolbar makes func. of the actionbar
         setSupportActionBar(toolbar);
+        //for bottom bar
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        //for fragments coming from bottom bar
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        //what user see first
+        replaceFragment(new HomeFragment());
+        //when click on bottom items
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
-
-
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.catalog:
+                    replaceFragment(new CatalogFragment());
+                    break;
+                case R.id.favourite:
+                    replaceFragment(new FavouriteFragment());
+                    break;
+                case R.id.shop_basket:
+                    replaceFragment(new BasketFragment());
+                    break;
+            }
             return true;
-
         });
+
     }
-    //putting the icons in the toolbar
+    //for choosing and replacing different fragments
+    //for MainActivity
+    private void replaceFragment (Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    //putting the icons in the top toolbar
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_bar_menu, menu);
+        getMenuInflater().inflate(R.menu.topbar_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
